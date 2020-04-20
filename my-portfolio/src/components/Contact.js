c732ac72c114c9f037060a64714339be05388d7a
“darsuabasi <abasibasiurua@gmail.com>
2020-04-20 17:08:10 -0400

    portfolio skeleton

---
+++ my-portfolio/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+
+  return (
+    <div className="contactMain"> 
+      <div className="contactSec">
+        <h1 className="styleContact"> 
+          Need My Skills? 
+        </h1>    
+      </div>
+    </div>  
+  )
+}
+
+
+export default Contact;
---
+++ my-portfolio/src/components/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Error = () => {
+
+  return (
+    <div className="errorHandling"> 
+        <div>
+            <div> 
+                <h1 className="message"> 
+                    Error Page 
+                </h1>
+            </div>
+        </div>
+    </div>  
+  )
+}
+
+
+export default Error;
