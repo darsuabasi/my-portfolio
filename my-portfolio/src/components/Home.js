97d49439b4ddd43a028d9f0f60709607bf3c9e14
“darsuabasi <abasibasiurua@gmail.com>
2020-05-16 20:40:57 -0400

    at the apple store saving at committing everything i have whohooo

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import Contact from '../components/Contact'
+import '../css/Home.css'
 
 const Home = () => {
 
@@ -8,6 +10,15 @@ const Home = () => {
         <h1 className="homeStyle"> 
           Welcome 
         </h1>
+
+        {/* <ul>
+          <li> <a href="#"> </a>Home </li>
+          <li> <a href="#"> </a>About </li>
+          <li> <a href="#"> </a>Projects </li>
+          <li> <a href="#"> </a>Blog </li>
+          <li> <a href="#"> </a> Photography </li>
+          <li> <a href="#"> </a> Contact</li>
+        </ul> */}
       </div>
     </div>  
   )
