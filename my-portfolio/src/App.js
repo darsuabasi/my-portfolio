ae76c154b847377212bda5433fe5d8ef8843ef4e
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-09-20 04:50:04 -0400

    add animation to landing page + install dependencies + libs

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter, useLocation } from 'react-router-dom'
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
-import Home from './components/Home'
+// import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Blog from './components/Blog'
@@ -11,48 +11,53 @@ import Photography from './components/Photography'
 import Contact from './components/Contact'
 import Error from './components/Error'
 import Landing from './components/Landing';
+import { AnimatePresence } from "framer-motion";
+
 
 
 function App() {
+  // const location = useLocation();
+
   return (
     <div className="my-portfolio">
       <BrowserRouter> 
-        <NavBar/> 
+        
+        
           <Switch>
 
-            <Route exact path={"/"}>
-              <Landing/>
-            </Route>
-            
-            <Route exact path={"/home"}>
-              <Home/>
-            </Route>
-    
-            <Route exact path={"/about"}>
-              <About/>
-            </Route>
-
-            <Route exact path={"/projects"}>
-              <Projects/>
-            </Route>
-
-
-            <Route exact path={"/blog"}>
-              <Blog/>
-            </Route>
-
-            <Route exact path={"/photography"}>
-              <Photography/>
-            </Route>
-
-            <Route exact path={"/contact"}>
-              <Contact/>
-            </Route> */}
-
-
-            <Route exact path={"*"}>
-              <Error />
-            </Route>
+            <AnimatePresence>
+                  {/* <Switch location={location} key={location.pathname}> */}
+                  <Route exact path={"/"}>
+                      <Landing/>
+                    </Route>
+        
+                    <Route exact path={"/about"}>
+                      <About/>
+                    </Route>
+                  {/* </Switch> */}
+                </AnimatePresence>
+        
+        
+        
+                <Route exact path={"/projects"}>
+                  <Projects/>
+                </Route>
+        
+                <Route exact path={"/blog"}>
+                  <Blog/>
+                </Route>
+        
+                <Route excat path={"/photography"}>
+                  <Photography/>
+                </Route>
+        
+                <Route exact path={"/contact"}>
+                  <Contact/>
+                </Route>
+        
+                <Route exact path={"*"}>
+                  <Error/>
+                </Route>
           </Switch>
         </BrowserRouter>
     </div>
