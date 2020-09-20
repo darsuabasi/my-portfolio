b9ca6b3408a0a41f7525cc687283df67de4a0086
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-09-20 07:10:52 -0400

    still fixing skeleton for about component

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import '../css/About.css'
-import Comments from '../components/Comments'
+import Comments from '../components/Comments';
+import NavBar from './NavBar';
+import Typing from 'react-typing-animation';
 
 const About = () => {
 
   return (
     <div className="AboutMain">
+      <NavBar/> 
       <div className="aboutSec"> 
-        <h1 className="world"> 
-          Uduakabasi's World 
-        </h1>
+        <Typing className="typingComp">
+          <p className="aboutInfoHead"> Get To Know Me</p>
+          <h1> Coder by day, judoka by night,</h1>
+          <h2> baddie on the weekends... </h2>
+          <h3> jk I code </h3>
+        </Typing>
 
-        <p className="aboutmepara">
-          Bachelors in Criminal Justice with a focus in Intelligence Analysis here to take the tech world by storm and also make it pretty. 
-          Fullstack web developer.
-          UX/UI and Backend enthusiast.
-          Oh, I do photography and talk skincare and advice....
-        </p>
+
+        <p className="aboutmepara">Currently enrolled at Pursuit, an intensive 12-month, Google-funded software engineering fellowship that has a 9% acceptance rate.</p>
+
+        <p className="aboutmepara"> - Nigerian </p>
+
+        <p className="aboutmepara"> - Bachelor of Science: Criminal Justice </p>
+  
+        <p className="aboutmepara"> - Concentration in Intelligence Analysis </p>
+
+        <p className="aboutmepara"> - Part-time judoka [pre-covoid]</p>
+
+        <p className="aboutmepara"> - UX/UI and Backend enthusiast</p>
+
+        <p className="aboutmepara"> - I'm a pretty rad photographer</p>
+
+        <p className="aboutmepara"> - Let's talk skincare</p>
+
+        <p className="aboutmepara"> - I occasionaly write stories on Wattpad.</p>
+
+        <p className="aboutmepara"> - Manga over Anime #DontJudge I'm a a reader</p>
+
 
       </div>
 
       <div>
-    <p>
-      Drop a comment or just cool vibes 
-    </p>
+        <p>
+          Drop a comment or just cool vibes... 
+        </p>
 
       <div className="commentSection"> 
-        <Comments/>
+        {/* <Comments/> */}
       </div>
 
       </div>
