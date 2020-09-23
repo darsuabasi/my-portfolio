0866469046dcf8c46857247b60ab6d8da682f194
“darsuabasi <abasibasiurua@gmail.com>
2020-09-23 17:48:16 -0400

    complete photography page

---
+++ my-portfolio/src/components/PhotoGallery/Images.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import lightingInSear from '../../galleryImages/lightingInSear.JPG';
+import lijMontree from '../../galleryImages/lijMontree.jpeg';
+import noniMontree from '../../galleryImages/noniMontree.JPG';
+import wentWithOzzy from '../../galleryImages/wentWithOzzy.JPG';
+import guggenhiem from '../../galleryImages/guggenhiem.JPG';
+import phillyPenetentiary from '../../galleryImages/phillyPenetentiary.jpeg';
+import lijAtMall from '../../galleryImages/lijAtMall.jpeg';
+import supremeLV from '../../galleryImages/supremeLV.JPG';
+import nigeria from '../../galleryImages/nigeria.jpeg';
+import food from '../../galleryImages/food.jpeg';
+import honeyInMontreeMarket from '../../galleryImages/honeyInMontreeMarket.JPG';
+import bonsaiInMontreePlantMus from '../../galleryImages/bonsaiInMontreePlantMus.jpeg';
+import artWallInGuggenhiem from '../../galleryImages/artWallInGuggenhiem.JPG';
+import guggenhiemArt from '../../galleryImages/guggenhiemArt.JPG';
+import beautifulPlantMontreePlantGarden from '../../galleryImages/beautifulPlantMontreePlantGarden.jpeg';
+import plantsInGuggenhiem from '../../galleryImages/plantsInGuggenhiem.JPG';
+import artInSHamptonMus from '../../galleryImages/artInSHamptonMus.jpeg';
+import noniInLavendarGarden from '../../galleryImages/noniInLavendarGarden.jpeg';
+import noniLavGardenWalk from '../../galleryImages/noniLavGardenWalk.jpeg';
+import playOnLightsDZ from '../../galleryImages/playOnLightsDZ.jpeg'
+
+
+const Images = [
+    lightingInSear, 
+    lijMontree, 
+    noniMontree, 
+    wentWithOzzy, 
+    guggenhiem, 
+    guggenhiemArt, 
+    phillyPenetentiary,
+    lijAtMall, 
+    supremeLV, 
+    playOnLightsDZ, 
+    nigeria,
+    food, 
+    honeyInMontreeMarket, 
+    bonsaiInMontreePlantMus, 
+    artWallInGuggenhiem, 
+    beautifulPlantMontreePlantGarden, 
+    plantsInGuggenhiem, 
+    artInSHamptonMus, 
+    noniInLavendarGarden, 
+    noniLavGardenWalk, 
+]
+
+export default Images
--- my-portfolio/src/components/Photography.js
+++ my-portfolio/src/components/Photography.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import Typing from 'react-typing-animation';
+import '../css/Photography.css';
+import GalleryGrid from './PhotoGallery/GalleryGrid';
+
 
 const Photography = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
 
   return (
     <div className="photographyMain"> 
-        <div className="photographySec">
+      <div className="navInPhotography">
+        <NavBar/> 
+      </div>
+
+        <div className="photographyHeading">
+          <Typing className="typeAnimation">
             <h1 className="stylePhotography"> 
-                Uduakabasi Shoots 
-            </h1>  
+              Uduakabasi Shoots
+            </h1>
+          </Typing>
+        </div>
+
+        <div className="sliderDiv">
+          <GalleryGrid/>
         </div>
     </div>  
   )
