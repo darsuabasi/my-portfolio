d73be52718663c4db953c691b6d8bbd1480ac7ec
darsuabasi <abasibasiurua@gmail.com>
2020-10-04 21:49:32 -0400

    new contact component created/trying to see if commits show verification

---
+++ my-portfolio/src/components2/NewContact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Typing from 'react-typing-animation';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram.svg';
+import linkedin from '../assets/linkedin.svg';
+import github from '../assets/github.svg';
+import '../css2/NewContact.css';
