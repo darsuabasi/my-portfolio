9da1e08d8ef6e5e216aae4adb3bc0c82ac1649ff
“darsuabasi <abasibasiurua@gmail.com>
2020-10-04 20:55:14 -0400

    complete responsiveness for photography page using bootstrap / made sass changes to about css

---
+++ my-portfolio/src/components2/NewPhotoGallery/Images2.js
@@ -0,0 +1,46 @@
+import lightingInSear from '../../galleryImages/lightingInSear.JPG';
+import lijMontree from '../../galleryImages/lijMontree.jpeg';
+import noniMontree from '../../galleryImages/noniMontree.JPG';
+import wentWithOzzy from '../../galleryImages/wentWithOzzy.JPG';
+import guggenhiem from '../../galleryImages/guggenhiem.JPG';
+import phillyPenetentiary from '../../galleryImages/phillyPenetentiary.jpeg';
+import lijAtMall from '../../galleryImages/lijAtMall.jpeg';
+import supremeLV from '../../galleryImages/supremeLV.JPG';
+import nigeria from '../../galleryImages/nigeria.jpeg';
+import food from '../../galleryImages/food.jpeg';
+import honeyInMontreeMarket from '../../galleryImages/honeyInMontreeMarket.JPG';
+import bonsaiInMontreePlantMus from '../../galleryImages/bonsaiInMontreePlantMus.jpeg';
+import artWallInGuggenhiem from '../../galleryImages/artWallInGuggenhiem.JPG';
+import guggenhiemArt from '../../galleryImages/guggenhiemArt.JPG';
+import beautifulPlantMontreePlantGarden from '../../galleryImages/beautifulPlantMontreePlantGarden.jpeg';
+import plantsInGuggenhiem from '../../galleryImages/plantsInGuggenhiem.JPG';
+import artInSHamptonMus from '../../galleryImages/artInSHamptonMus.jpeg';
+import noniInLavendarGarden from '../../galleryImages/noniInLavendarGarden.jpeg';
+import noniLavGardenWalk from '../../galleryImages/noniLavGardenWalk.jpeg';
+import playOnLightsDZ from '../../galleryImages/playOnLightsDZ.jpeg'
+
+
+const Images = [
+    guggenhiemArt, 
+    lijAtMall, 
+    wentWithOzzy, 
+    noniLavGardenWalk, 
+    plantsInGuggenhiem, 
+    noniMontree, 
+    phillyPenetentiary,
+    supremeLV, 
+    artInSHamptonMus, 
+    artWallInGuggenhiem, 
+    noniInLavendarGarden, 
+    guggenhiem, 
+    food, 
+    nigeria,
+    beautifulPlantMontreePlantGarden, 
+    lightingInSear,
+    lijMontree, 
+    playOnLightsDZ, 
+    honeyInMontreeMarket, 
+    bonsaiInMontreePlantMus, 
+]
+
+export default Images
