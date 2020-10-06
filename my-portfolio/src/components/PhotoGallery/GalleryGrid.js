e5be789b7d36e10e59e9947255f35c13c996c77d
darsuabasi <abasibasiurua@gmail.com>
2020-10-06 16:39:05 -0400

    preserve/comment out old work with manual responsiveness

--- my-portfolio/src/components/PhotoGallery/GalleryGrid.js
+++ my-portfolio/src/components/PhotoGallery/GalleryGrid.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Images from './Images';
-import './css/GalleryGrid.css'
-import { motion } from 'framer-motion';
+// import React from 'react';
+// import Images from './Images';
+// import './css/GalleryGrid.css'
+// import { motion } from 'framer-motion';
 
-const GalleryGrid = ({ }) => {
-  return (
-    <div className="img-grid">
-      {Images && Images.map(Images => (
-        <motion.div className="img-wrap" 
-          layout
-          whileHover={{ opacity: 0.9 }}s
-        >
-          <motion.img className="galleryImageStyling" style={{width:"550px", height:"500px", borderRadius:"0.5rem"}} src={Images} alt="Gallery Photo"
-            initial={{ opacity: 0.5 }}
-            animate={{ opacity: 0.9 }}
-            transition={{ delay: 2 }}
-          />
-        </motion.div>
-      ))}
-    </div>
-  )
-}
+// const GalleryGrid = ({ }) => {
+//   return (
+//     <div className="img-grid">
+//       {Images && Images.map(Images => (
+//         <motion.div className="img-wrap" 
+//           layout
+//           whileHover={{ opacity: 0.9 }}s
+//         >
+//           <motion.img className="galleryImageStyling" style={{width:"550px", height:"500px", borderRadius:"1.5rem"}} src={Images} alt="Gallery Photo"
+//             initial={{ opacity: 0.5 }}
+//             animate={{ opacity: 0.9 }}
+//             transition={{ delay: 2 }}
+//           />
+//         </motion.div>
+//       ))}
+//     </div>
+//   )
+// }
 
-export default GalleryGrid;
+// export default GalleryGrid;
