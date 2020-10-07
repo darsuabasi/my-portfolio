c905e32c2ab0c440d44d634a05226f3714657e02
darsuabasi <abasibasiurua@gmail.com>
2020-10-06 21:02:03 -0400

    comment out initial landing component

--- my-portfolio/src/components/Landing.js
+++ my-portfolio/src/components/Landing.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-// import twitter from '../assets/twitter.svg';
-// import instagram from '../assets/instagram.svg';
-// import linkedin from '../assets/linkedin.svg';
-// import github from '../assets/github.svg';
-import Typing from 'react-typing-animation';
-import { pageTransition, pageVariants } from "../util/framerStyles";
-import { motion } from "framer-motion";
-import '../css/Landing.css';
+// import React from 'react';
+// import { useHistory } from 'react-router-dom';
+// import Typing from 'react-typing-animation';
+// import { pageTransition, pageVariants } from "../util/framerStyles";
+// import { motion } from "framer-motion";
+// import '../css/Landing.css';
 
 
 
-const Landing = ({ Component, pageProps, router }) => {
-    const history = useHistory();
-    return (
-        <div className="majorDiv"> 
-        <motion.div
+// const Landing = ({ Component, pageProps, router }) => {
+//     const history = useHistory();
+//     return (
+//         <div className="majorDiv"> 
+//         <motion.div
                 // key={router.route}
-                initial="initial"
-                animate="in"
-                exit="out"
-                transition={pageTransition}
-                variants={pageVariants}
-                >
-                <div {...pageProps} className="landingMain"> 
-                    <div className="landingBio">
-                        <Typing className="typeingAni">
-                            <p className="landingInfoHead"> Hi, I'm Uduakabasi.</p>
-                            <h1 className="landingInfoSub">  Web Engineer,</h1>
-                            <h2 className="landingInfoSub"> Fullstack,</h2>
-                            <h3 className="landingInfoSub"> && more.</h3>
-                        </Typing>
+                // initial="initial"
+                // animate="in"
+                // exit="out"
+                // transition={pageTransition}
+                // variants={pageVariants}
+                // >
+                // <div {...pageProps} className="landingMain"> 
+                //     <div className="landingBio">
+                //         <Typing className="typeingAni">
+                //             <p className="landingInfoHead"> Hi, I'm Uduakabasi.</p>
+                //             <h1 className="landingInfoSub">  Web Engineer,</h1>
+                //             <h2 className="landingInfoSub"> Fullstack,</h2>
+                //             <h3 className="landingInfoSub"> && more.</h3>
+                //         </Typing>
 
-                    <div className="btn-div">
-                        <button className="enterWorldBtn" onClick={() => history.push(`/about${history.location.pathname}`)}>Enter </button>
-                        <button className="enterContactBtn" onClick={() => history.push(`/contact${history.location.pathname}`)}>Contact</button>
-                    </div>
-                </div>
+                //     <div className="btn-div">
+                //         <button className="enterWorldBtn" onClick={() => history.push(`/about${history.location.pathname}`)}>Enter </button>
+                //         <button className="enterContactBtn" onClick={() => history.push(`/contact${history.location.pathname}`)}>Contact</button>
+                //     </div>
+                // </div>
 
             
 
@@ -73,101 +69,13 @@ const Landing = ({ Component, pageProps, router }) => {
                 
                 </ul>
             </div> */}
-        </div>
-            </motion.div>
-            </div>
-   )
-}
+//         </div>
+//             </motion.div>
+//             </div>
+//    )
+// }
 
-export default Landing
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-//     // const location = useLocation();
-  
-//     return (
-//       <div className="my-portfolio">
-//         <NavBar/>
-//         <Switch>
-  
-//         {/* <AnimatePresence> */}
-//             {/* <Switch location={location} key={location.pathname}> */}
-//               <Route exact path={"/"}>
-//                 <Landing/>
-//               </Route>
-  
-//               <Route exact path to={"/home"}>
-//                 <Home/>
-//               </Route>
-  
-//               <Route exact path to={"/about"}>
-//                 <About/>
-//               </Route>
-//             {/* </Switch> */}
-//           {/* </AnimatePresence> */}
-  
-  
-  
-//           <Route exact path={"/projects"}>
-//             <Projects/>
-//           </Route>
-  
-//           <Route exact path to={"/blog"}>
-//             <Blog/>
-//           </Route>
-  
-//           <Route excat path to={"/photography"}>
-//             <Photography/>
-//           </Route>
-  
-//           <Route exact path to={"/contact"}>
-//             <Contact/>
-//           </Route>
-  
-//           <Route exact path to={"*"}>
-//             <Error/>
-//           </Route>
-  
-//         </Switch>
-  
-//       </div>
-      
-//     );
-//   }
-  
-//   export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-             
-        
-              
+// export default Landing              
         
               
         
