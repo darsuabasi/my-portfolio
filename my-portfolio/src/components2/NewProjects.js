8c3bc6895a547870d06fbaa2f5b669caad2387ab
darsuabasi <abasibasiurua@gmail.com>
2021-02-05 08:20:34 -0500

    change design layout of projects component

--- my-portfolio/src/components2/NewProjects.js
+++ my-portfolio/src/components2/NewProjects.js
@@ -12,49 +12,71 @@ import '../css2/NewProjects.css';
 import zoovioApp from '../assets/zoovioApp.png';
 // import cloutApp from '../assets/cloutApp.png';
 import aatgApp from '../assets/aatgApp.png';
-// import pinterestApp from '../assets/pinterestApp.jpeg';
-import pinterestApp2 from '../assets/pinterestApp2.png';
+import lifetrestApp from '../assets/lifetrestApp.png';
+import lifetrestLandingpage from '../projectASSETS/lifetrestLandingpage.gif';
+import aatgLanding from '../projectASSETS/aatgLanding.png';
 // import letsgoApp from '../assets/letsgoApp.png';
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 
 const NewProject = () => {
     return (
-        <Container fluid className="projectMain" style={{width: '100vw'}}>
-
+        <Container fluid className="projectMain">
             <Row className="subContain">
 
-            <Col>
-                <Typing className="sheCodes">
-                    <h1 className="styleProj"> 
-                        Uduakabasi Codes
-                    </h1>
-                </Typing>
-            </Col>
-            <Col>
-                <CardDeck sm={4} className="allTheCards"> 
+                <Col>
+                    <Typing className="sheCodes">
+                        <h1 className="styleProj"> 
+                            Uduakabasi Codes
+                        </h1>
+                    </Typing>
+                </Col>
+                <Col>
+                    <CardDeck sm={4} className="allTheCards"> 
 
-                <div className="projectThree">
-                    <a href="https://lifetrest.netlify.app/" target="_blank"> 
-                        <img className="appScreenshots" alt="pinterestApp" style={{width:"350px", height:"192px"}} src={pinterestApp2} />
-                    </a>
-                  <h2 className="projectName top"> Lifetrest</h2>
-                  <div className="codeDiv">
-                    <a className="appLink" href="https://lifetrest.netlify.app/" target="_blank"> Happy Pinning <span role="img" alt="smile emoji"> 😁 </span>  </a>
-                    <a className="checkOutTheCode" href="https://github.com/darsuabasi/CTAPintrest" target="_blank">Check out the code.</a>
-                  </div>
-                  <p className="bioForTheProject">
-                  Fullstack React app. Users can signup/login through Google Firebase, create pins and boards with images.
-                  Users can view their boards and pins on their profile. New pins can aslo be added to previously created boards.
-                  Boards can be deleted which will in turn remove all pins in that board. Modal is utilized so that images are the focus.  
-                  </p>
-                  <p className="whatsNext">
-                  What's Next: 
-                  The ability to delete pins.
-                  {/* Users will be able to search pins by hashtag. */}
-                  The ability to like pins which will then display in a generated "Likes" board.
-                  </p>
-                </div>
+                        <div className="projectThree">
+                            <a href="https://lifetrest.netlify.app/" target="_blank"> 
+                                <img loading="lazy" className="appScreenshots" alt="pinterestApp" class="image__img" src={lifetrestApp} />
+                            </a>
+
+                            <div class="image__overlay image__overlay--primary">
+                                <NavLink class="image__title" className="nav-link nav-selected" exact to="/projects/lifetrest">
+                                Lifetrest
+                                </NavLink>
+                                <p class="image__description">
+                                    In an alternate universe, Lifetrest would dominate but it's a good thing Pinterest exists
+                                    because I don't think this would go public for another year! Ha. I can tell you all the things I love about Pinterest but then we'd be here for a long time... 
+                                    so I decided to recreate it as part of my Comprehensive Technical Assessment but even after 
+                                    months, I couldn't let it go. Watching it come to life was nothing short of a gritty yet 
+                                    beautiful journey.
+                                </p>
+                                <p class="image__description">
+                                Built with React.js, Node.js with Express, and PostgreSQL.
+                                </p>
+
+                                <a style={{fontVariant:'small-caps', fontSize:'28px'}} className="image__link" href="https://github.com/darsuabasi/CTAPintrest" target="_blank"> The Code </a> 
+                                <a style={{fontVariant:'small-caps', fontSize:'24px'}} className="image__link" href="https://lifetrest.netlify.app/" target="_blank"> Live Site</a>
+                                <NavLink style={{fontVariant:'small-caps', fontSize:'20px'}} class="image__title" className="nav-link nav-selected" exact to="/projects/lifetrest">
+                                Click Me to View More
+                                </NavLink>
+                            </div>
+                        {/* <h2 className="projectName top"> Lifetrest</h2>
+                        <div className="codeDiv">
+                            <a className="appLink" href="https://lifetrest.netlify.app/" target="_blank"> Happy Pinning <span role="img" alt="smile emoji"> 😁 </span>  </a>
+                            <a className="checkOutTheCode" href="https://github.com/darsuabasi/CTAPintrest" target="_blank">Check out the code.</a>
+                        </div> */}
+                        {/* <p className="bioForTheProject">
+                        Fullstack React app. Users can signup/login through Google Firebase, create pins and boards with images.
+                        Users can view their boards and pins on their profile. New pins can aslo be added to previously created boards.
+                        Boards can be deleted which will in turn remove all pins in that board. Modal is utilized so that images are the focus.  
+                        </p> */}
+                            {/* <p className="whatsNext">
+                                What's Next: 
+                                The ability to delete pins.
+                    
+                                The ability to like pins which will then display in a generated "Likes" board.
+                            </p> */}
+                        </div>
                     {/* <Card className="fullCard" style={{height: '30rem', width: '24rem'}}>
                         <Card.Img className="projectPic" variant="top" alt="zoovioApp" style={{ width:"350px", height:"200px"}} src={zoovioApp} />
                         <Card.Body>
@@ -79,10 +101,34 @@ const NewProject = () => {
 
                     <div className="projectOne">
                         <a href="https://zoovio.netlify.app/" target="_blank"> 
-                            <img className="appScreenshots" alt="zoovioApp" href="https://zoovio.netlify.app/" target="_blank" style={{width:"350px", height:"192px"}} src={zoovioApp}/>
+                            <img loading="lazy" className="appScreenshots" alt="zoovioApp" class="image__img" href="https://zoovio.netlify.app/" target="_blank" src={zoovioApp}/>
                         </a>
-                  <h2 className="projectName top"> ZooVio</h2>
-                    <div className="codeDiv">
+                  {/* <h2 className="projectName top"> ZooVio</h2> */}
+                        <div class="image__overlay image__overlay--primary">
+                            <NavLink class="image__title" className="nav-link nav-selected" exact to="/projects/zoovio">
+                                ZooVio
+                                </NavLink>
+                                
+
+                                <p class="image__description">
+                                    Too many pets, not enough time. Welcome to ZooVio, you're one stop shop and 
+                                    your pets will thank you. Need to create a task? Check and you can view it
+                                    in our cute calander. Need to locate a new pet shop because the one 2 blocks 
+                                    down shut down? We've got you covered. Still super nervous about setting about 
+                                    appointments because, well... Covid? Say no more, set up a virtual appointment 
+                                    with your pets' vetenarian today.
+                                </p>
+                                <p class="image__description"> Built with React, Redux, Node.js + Express.js., PostgreSQL
+                                </p>
+
+                                <a style={{fontVariant:'small-caps', fontSize:'28px'}} className="image__link" href="https://github.com/Zoovio-app/zoovio" target="_blank"> The Code </a> 
+                                <a style={{fontVariant:'small-caps', fontSize:'24px'}} className="image__link" href="https://zoovio.netlify.app/" target="_blank"> Live Site</a>
+                                <NavLink style={{fontVariant:'small-caps', fontSize:'20px'}} class="image__title" className="nav-link nav-selected" exact to="/projects/zoovio">
+                                Click Me to View More
+                                </NavLink>
+
+                            </div>
+                    {/* <div className="codeDiv">
                       <a className="appLink" href="https://zoovio.netlify.app/" target="_blank">Keep Track of Your Pets! <span role="img" alt="smile emoji"> 😁 </span> </a>
                       <a className="checkOutTheCode" href="https://github.com/darsuabasi/zoovio" target="_blank">    Check out the code.</a>
                     </div>
@@ -99,8 +145,10 @@ const NewProject = () => {
                   Expanding servives for more than just vetnarians. Looking into more complex verification
                   system for doctor sign-ups. Ending/Declining incoming virtual appointment feature. Document upload
                   for doctor's portal and payment method for services.
-                  </p>
+                  </p> */}
                 </div>
+
+
                 {/* </Col> */}
 
                 {/* <Col> */}
@@ -129,9 +177,21 @@ const NewProject = () => {
 
                 <div className="projectTwo">
                     <a href="https://github.com/Ohidurr/AllAboutTheGreen" target="_blank">
-                        <img className="appScreenshots" alt="allAboutTheGreenApp" style={{width:"350px", height:"192px"}} src={aatgApp}/>
+                        <img loading="lazy" className="appScreenshots" alt="allAboutTheGreenApp" class="image__img" src={aatgLanding}/>
                     </a>
-                  <h2 className="projectName top"> All About the Green </h2>
+
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">All About the Green</div>
+                        <p class="image__description">
+                        A beginner friendly plant app created for Pursuit's 48 hour Hackathon, sponsored by CodeAcademy. Users are able to look up information on various plants, upload photos to the community forum and scan plant images to recieve plant identification accuracy report. Won MVP Award from CodeAcademy judges.
+                        </p>
+
+                        <a style={{fontVariant:'small-caps', fontSize:'28px'}} className="image__link" href="https://github.com/Ohidurr/AllAboutTheGreen" target="_blank"> The Code </a>
+                        <NavLink style={{fontVariant:'small-caps', fontSize:'20px'}} class="image__title" className="nav-link nav-selected" exact to="/projects/all-about-the-green">
+                            Click Me to View More
+                        </NavLink>
+                    </div>
+                  {/* <h2 className="projectName top"> All About the Green </h2>
                   <div className="codeDiv"> 
                     <a className="checkOutTheCode" href="https://github.com/darsuabasi/AllAboutTheGreen" target="_blank"> Check out the code.</a>
                   </div>
@@ -147,7 +207,7 @@ const NewProject = () => {
                   What's Next: 
                   Get application deployed. Debug inconsistancies with Plant.Id API as well as cleanining up the 
                   design so that it's mobile friendly.
-                  </p>
+                  </p> */}
                 </div>
 
                 {/* <Col> */}
